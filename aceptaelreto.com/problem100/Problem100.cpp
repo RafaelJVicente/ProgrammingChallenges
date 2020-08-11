986e5ce59a7a6c6e169685e965f596416c5be9fb
Rafael J. Vicente <rafaelj.vicente@gmail.com>
2020-08-11 22:12:51 +0200

    Added aceptaelreto.com/problem100 V1.3

--- aceptaelreto.com/problem100/Problem100.cpp
+++ aceptaelreto.com/problem100/Problem100.cpp
@@ -7,34 +7,55 @@
 
 #include <iostream>
 #include <algorithm>
-#include <map>
 #include <list>
 
 using namespace std;
 
+void read_int(int &number)
+{
+    register int c;
+    number = 0;
+    c = getchar();
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+}
 
 /**
  * Main function
  */
 int main()
 {
+    const int KAPREKAR = 6174;
     const int MAX_S = 4;
 
-    int n_numbers;
-    cin >> n_numbers;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    map<int, int> cache_map;
+    int n_numbers;
+    //cin >> n_numbers;
+    read_int(n_numbers);
+
+    const int CACHE_MAX_S = 10000;
+    const int CACHE_DEF_VAL = -1;
+    int iter_cache_arr[CACHE_MAX_S];
+    fill(iter_cache_arr, iter_cache_arr + CACHE_MAX_S, CACHE_DEF_VAL);
+
+    // Init defaults
+    iter_cache_arr[KAPREKAR] = 0;
+    for (int i = 0;i < CACHE_MAX_S; i+=1111)
+        iter_cache_arr[i] = 8;
 
     int arr[MAX_S];
     for (int i = 0; i < n_numbers; ++i)
     {
-
         int num;
-        cin >> num;
+        //cin >> num;
+        read_int(num);
 
-        if (num % 1111 == 0)
+        if (iter_cache_arr[num] != CACHE_DEF_VAL)
         {
-            cout << 8 << endl;
+            cout << iter_cache_arr[num] << "\n";
             continue;
         }
         else
@@ -54,10 +75,9 @@ int main()
                 num = arr[3] * 1000 + arr[2] * 100 + arr[1] * 10 + arr[0] * 1;
 
                 // Search for cached
-                auto it = cache_map.find(num);
-                if (it != cache_map.end())
+                if (iter_cache_arr[num] != CACHE_DEF_VAL)
                 {
-                    iteration += it->second;
+                    iteration += iter_cache_arr[num];
                     break;
                 }
 
@@ -73,13 +93,13 @@ int main()
             int aux_iteration = iteration;
             for (const auto& cached_num : num_list)
             {
-                cache_map[cached_num] = aux_iteration--;
+                iter_cache_arr[cached_num] = aux_iteration--;
             }
 
-            cout << iteration << endl;
+            cout << iteration << '\n';
         }
     }
 
     return 0;
 }
-
+ 
